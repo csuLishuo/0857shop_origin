c5561d337c1ca8e60d9f599a24d0fff2e5f999e2
csuLishuo <502631975@qq.com>
2019-06-16 12:11:55 +0800

    center

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -3,10 +3,6 @@
     //$px为需要转换的字号
     @return $px * 1 / 100 * 1rem;
   }
-  img,input{
-    vertical-align: middle;
-    display: block;
-  }
   .home-container{
     padding-bottom: px2rem(50);
     .banner{
@@ -18,6 +14,88 @@
         height: 100%;
       }
     }
+    .search-box{
+        background: #fff;
+        padding: px2rem(10) px2rem(30);
+      .input-box{
+        background: #eee;
+        border-radius: px2rem(29);
+        padding-left: px2rem(50);
+        height: px2rem(58);
+        width: 100%;
+        position: relative;
+        overflow: hidden;
+        img{
+          width: px2rem(25);
+          height: px2rem(25);
+          position: absolute;
+          left: px2rem(16);
+          top: px2rem(17);
+        }
+        input{
+          width: 100%;
+          line-height: px2rem(58);
+          font-size: px2rem(25);
+          background: transparent;
+        }
+      }
+    }
+    .fixed-box{
+      z-index: 100;
+      position: fixed;
+      top: 50%;
+      right: px2rem(30);
+      width: px2rem(77);
+	    height: px2rem(164);
+	    background-color: #282a2e;
+	    border-radius: px2rem(39);
+	    opacity: 0.7;
+      display: flex;
+      flex-direction: column;
+      flex-wrap: nowrap;
+      align-items: center;
+      justify-content: space-around;
+      .wrapper{
+        display: flex;
+        align-items: center;
+        justify-content: center; 
+        width: px2rem(43);
+        height: 50%;
+        .img-box{
+          width: px2rem(43);
+          height: px2rem(39);
+          img{
+            width: 100%;
+            height: 100%;
+          }
+        }
+        &.wrapper-1{
+          .img-box{
+            position: relative;
+          }
+          span{
+            position: absolute;
+            top: px2rem(-10);
+            right: px2rem(-10);
+            height: px2rem(30);
+            line-height: px2rem(30);
+            border-radius: px2rem(15);
+            min-width: px2rem(30);
+            color: #fff;
+            text-align: center;
+            background: #ff0000;
+            font-size: px2rem(24)
+          }
+        }
+        &.wrapper-2{
+          .img-box{
+            width: px2rem(31);
+            height: px2rem(34);
+          }
+          border-top: px2rem(1) solid rgba(255,255,255,0.3);
+        }
+      }
+    }
     .area-3{
       background: #fff;
       .scroll-box{
@@ -27,6 +105,7 @@
         overflow-x: scroll;
         -webkit-overflow-scrolling:touch;
         width: 100%;
+        border-bottom: 1px solid #ececec;
         .item{
           font-size: px2rem(28);
           line-height: px2rem(88);
@@ -81,6 +160,25 @@
   <div class="home-container">
     <div class="banner">
       <img src="../images/icon1.png" alt="">
+    </div>
+    <div class="search-box">
+      <div class="input-box">
+        <img src="../images/icon6.png" alt="">
+        <input type="text">
+      </div>
+    </div>
+    <div class="fixed-box">
+      <div class="wrapper wrapper-1">
+        <div class="img-box">
+          <img src="../images/icon8.png" alt="">
+          <span>1</span>
+        </div>
+      </div>
+      <div class="wrapper wrapper-2">
+        <div class="img-box">
+          <img src="../images/icon9.png" alt="">
+        </div>
+      </div>
     </div>
     <div class="area-3">
       <div class="scroll-box">
--- src/pages/myCenter.vue
+++ src/pages/myCenter.vue
@@ -1,6 +1,108 @@
+<style lang="scss" scoped>
+  @function px2rem($px) {
+    //$px为需要转换的字号
+    @return $px * 1 / 100 * 1rem;
+  }
+  .area-1{
+    width: 100%;
+    height: px2rem(418);
+    background-image: url("../images/img1.png");
+    background-size: 100% 100%;
+    background-repeat: no-repeat;
+    .wrapper{
+      padding: px2rem(96) px2rem(40) 0;
+      display: flex;
+      flex-direction: row;
+      flex-wrap: nowrap;
+      align-items: center;
+      justify-content: flex-start;
+      .portrait-box{
+        width: px2rem(153);
+        height: px2rem(153);
+        border-radius: 50%;
+        overflow: hidden;
+        margin-right: px2rem(20);
+        img{
+          width: 100%;
+          height: 100%;
+        }
+      }
+      .name{
+        font-size: px2rem(38);
+        color: #fff;
+      }
+    }
+  }
+  .area-2{
+    .wrapper{
+      padding: 0 px2rem(38);
+      height: px2rem(98);
+      line-height: px2rem(98);
+      background: #fff;
+      display: flex;
+      flex-direction: row;
+      flex-wrap: nowrap;
+      align-items: center;
+      justify-content: space-between;
+      .left-box{
+        width: 80%;
+        display: flex;
+        flex-direction: row;
+        flex-wrap: nowrap;
+        align-items: center;
+        justify-content: flex-start;
+        img{
+          width: px2rem(38);
+          height: px2rem(38);
+          margin-right: px2rem(27);
+        }
+        .name{
+          font-size: px2rem(30);
+        }
+      }
+      .right-box{
+        width: px2rem(13);
+        height: px2rem(24);
+        img{
+          width: 100%;
+          height: 100%;
+        }
+      }
+    }
+  }
+</style>
 <template>
   <div class="myCenter-container">
-    myCenter
+    <div class="area-1">
+      <div class="wrapper">
+        <div class="portrait-box">
+          <img src="../images/icon2.png" alt="">
+        </div>
+        <div class="name">
+          name
+        </div>
+      </div>
+    </div>
+    <div class="area-2">
+      <div class="wrapper">
+        <div class="left-box">
+          <img src="../images/icon4.png" alt="">
+          <div class="name">我的仓库</div>
+        </div>
+        <div class="right-box">
+          <img src="../images/icon7.png" alt="">
+        </div>
+      </div>
+      <div class="wrapper">
+        <div class="left-box">
+          <img src="../images/icon5.png" alt="">
+          <div class="name">我的消息</div>
+        </div>
+        <div class="right-box">
+          <img src="../images/icon7.png" alt="">
+        </div>
+      </div>
+    </div>
     <tabbar :activeIndex="1"></tabbar>
   </div>
 </template>
@@ -33,9 +135,4 @@ export default {
   }
 }
 </script>
-<style lang="scss" scoped>
-  @function px2rem($px) {
-    //$px为需要转换的字号
-    @return $px * 1 / 100 * 1rem;
-  }
-</style>
+
